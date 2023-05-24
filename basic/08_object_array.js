8fdf9b85b594ede8ea94435cb714c6d726a32f92
yoon88 <ysmdudwo@hotmail.com>
2023-05-24 11:00:00 +0900

    java

--- basic/08_object_array.js
+++ basic/08_object_array.js
@@ -7,8 +7,12 @@
     arr1[1] = 'hello' //첫번째 방법
     console.log(arr1)
 
+    console.log('------------------')
+
     const arr2 = [1,3,5,7,9,'안녕',{},true]
     console.log(arr2)
+
+    console.log('------------------')
 
     const arr3 = [
         'a',
@@ -18,8 +22,57 @@
 
     console.log(arr3)
 
+    console.log('------------------')
+
     //접근방법
     console.log(arr2[5])
     // apple에 접근하기 배열은 인덱스번호로, 객체 안은 '.'으로
     console.log(arr3[1].model)
+    console.log('------------------')
 }
+
+// 2. 배열 출력
+{
+    const books = ['html','css','javascript']
+    console.log(books)
+    console.log(books[books])
+    console.log(books.length) //3
+    console.log(books[0]) //html 배열의 첫 요소 반환
+    console.log(books[books.length - 1]) // 배열의 마지막 요소 반환
+
+    // for문
+    for(let i = 0; i < books.length; i++) {
+        console.log(books[i])
+    }
+
+    console.log('------------------')
+
+    // for of---for문을 간단하게 for of로 나타낼수 있음. 대신 for문을 사용하면 'break', 이중for문 사용 가능
+    for(let book of books) {
+        console.log(book)
+    }
+
+    console.log('------------------')
+
+    // for in
+    for(let key in books) {
+        console.log(key)
+    }
+
+    for(let i in books) {
+        console.log(books[i])
+    }
+
+    console.log('------------------')
+
+    // forEach
+    books.forEach(function(){
+        //
+    })
+    books.forEach((book)=>{console.log('📗'+ book)})
+
+    //map : 새로운 배열을 리턴
+    const newBooks = books.map(item => `${item} 🎃`)
+    console.log(newBooks)
+}
+
---
+++ basic/function.js
@@ -0,0 +1,72 @@
+// 1.함수 - 동작....
+console.log('안녕하세요')
+
+// 함수의 선언과 호출
+function myFunc() {
+    console.log('안녕하세요')
+}
+
+myFunc()
+
+//매개변수 Parameter
+function printMessage(message) {
+    console.log(message)
+}
+
+
+printMessage('호호호')
+printMessage('Hello Javascript')
+
+//기본 매개변수
+function showInfo(title, name = '스티브 잡스') {
+    console.log(`${title} by ${name}`)
+}
+
+showInfo('게르리카','피카소')
+showInfo('풍경')
+
+// 스코프 scope
+let movie = '하하하' //전 영역에서 유효함
+
+function printMovie() { //함수의 스코프는 중괄호 안에서만 유효함 
+    let myMovie = '생활의 발견'
+    console.log(myMovie)
+    console.log(movie)
+}
+
+printMovie()
+// console.log(myMovie)
+
+
+// return
+function plus(a,b) {
+    console.log(a+b)
+    return a+b
+}
+
+const result = plus(10,12)
+console.log(result)
+
+function plus(a,b) {
+    return a+b
+    console.log(a+b)
+}  //이렇게 쓰면 실행 안됨 함수를 빨리 끝내고싶을때 사용 가능
+
+// Arrow function (화살표 함수)
+const sum = add(5,15)
+console.log(sum)
+
+function add(a,b) {
+    return a+b
+}
+
+const minus = function(a,b) {
+    return a - b
+}
+
+const sum2 = minus(20,10)
+console.log(sum2)
+
+const minus2 = (a,b) => a-b //function생략하는대신(), {}생락하는대신 =>
+const sum3 = minus2(50,25)
+console.log(sum3)
